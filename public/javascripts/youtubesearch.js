8d71da2decbba937e40944e3f0e3e91a14bd5208
Adrian lopez de la cruz <noknows6@gmail.com>
2017-05-07 20:33:27 -0500

    avance de resultados

--- public/javascripts/youtubesearch.js
+++ public/javascripts/youtubesearch.js
@@ -1,25 +1,57 @@
 var suggestCallBack; // global var for autocomplete jsonp
 
 $(document).ready(function () {
-    $("#search").autocomplete({
-        source: function(request, response) {       
+  var search = jQuery("#search");
+    search.autocomplete({
+        source: function(request, response) {   
+         
         var result = youtube_search( request.term );
         result.execute( function( server ){
-          console.log( server);
+          var items = server.items;
+          response( items );
+          
         });
         
         },
-    });// end autocomplete
+    })
+
+.autocomplete( "instance" )._renderItem = function( ul, item ) {
+    if(  !ul.hasClass('list-group')){
+      ul.addClass('list-group');
+    }
+    var snipped = item.snippet
+    console.log( item);
+    var title = snipped.title;
+    var image = snipped.thumbnails.medium.url;
+    var img = $('<img  class="img-responsive img-thumbnail">'); //Equivalent: $(document.createElement('img'))
+    img.attr('src',image);
+
+  
+    return $( "<li class='list-group-item' >" )
+    .append( "<a>" + title  + "</a>" ).append(img)
+    .appendTo( ul );
+};
+
+/*    search.data('autocomplete')._renderItem = function(ul, item) {
+        return $('<li></li>')
+            .data('item.autocomplete', item)
+            .append('<a><img src="' + item.avatar + '" />' + item.name + '<br></a>')
+            .appendTo(ul);
+    };*/
+
+
+
+    // end autocomplete
 });
 
 
-var youtube_search = function( search  ){
+var youtube_search = function( search, response ){
 
    var resquest =   gapi.client.youtube.search.list({
             part: "snippet",
             type: "video",
             q: encodeURIComponent( search  ).replace(/%20/g, "+"),
-            maxResults: 3,
+            maxResults: 10,
             order: "viewCount",
             publishedAfter: "2015-01-01T00:00:00Z"
       });
