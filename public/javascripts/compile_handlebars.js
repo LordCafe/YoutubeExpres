dff8e6ab04b5b74e40bbcebc8b322f61c10d57ad
Adrian lopez de la cruz <noknows6@gmail.com>
2017-05-10 00:31:38 -0500

    read file

--- public/javascripts/compile_handlebars.js
+++ public/javascripts/compile_handlebars.js
@@ -1,9 +1,13 @@
 var scripts = document.getElementsByTagName( 'script' );
-
+console.log( scripts);
 for(var i in scripts){
  var type  = scripts[i].type ;
+
+ var script = scripts[i];
  var id = scripts[i].id;
- if(type === 'handlebars'){
+//console.log( i );
+ if(type =='handlebars' && id ){
+ 	console.log( jQuery('#'+id ).html());
    Handlebars.template = Handlebars.template || [];
    Handlebars.template[id]
  }
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var handlebars = [{file : '/javascripts/thumbnail_youtube.hbs', id : 'thumbnail'}];
-  res.render('index', { title: 'Express' , handlebars : handlebars  });
+  
+fs = require('fs');
+//var cheerio = require('cheerio');
+var file =fs.readFileSync( __dirname + '/../public/javascripts/thumbnail_youtube.hbs', 'UTF-8');
+ file = file.toString();
+ var handlebars = [{file : '/javascripts/thumbnail_youtube.hbs', id : 'thumbnail', data : file 	}];
+ res.render('index', { title: 'Express' , handlebars : handlebars  });
 });
 
 module.exports = router;
